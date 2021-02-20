9b4e6d2a696818993025fb607a27ce40fcea227b
youvegotnigel <nigeldave19@gmail.com>
2021-02-20 22:46:29 +0530

    add test case to take element screenshot

--- src/test/java/selenium4_tests/PageScreenshot.java
+++ src/test/java/selenium4_tests/PageScreenshot.java
@@ -2,22 +2,25 @@ package selenium4_tests;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.apache.commons.io.FileUtils;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.io.FileHandler;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import java.io.File;
 import java.io.IOException;
+import java.time.Duration;
 
 public class PageScreenshot {
 
     WebDriver driver;
+
+    private By logo = By.id("nav-logo");
 
     @BeforeClass
     public void setUp () 	{
@@ -37,6 +40,22 @@ public class PageScreenshot {
     public void takePageScreenshot () throws IOException {
         File source = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
         FileUtils.copyFile(source, new File("Amazon Screenshot.png"));
+    }
+
+    @Test
+    public void takeElementScreenshot() throws IOException {
+
+        /** WebDriverWait method new parameter introduced in in selenium 4
+         *  Old parameter with only time is deprecated.
+         * */
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(logo));
+
+        WebElement amazonLogo = driver.findElement(logo);
+
+        File screenshot = amazonLogo.getScreenshotAs(OutputType.FILE);
+        FileUtils.copyFile(screenshot, new File("Amazon Logo.png"));
+
     }
 
     @AfterClass
