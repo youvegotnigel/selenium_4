c930ec971e4e230b45a20aa4ff909d0b040b4585
youvegotnigel <nigeldave19@gmail.com>
2021-02-20 21:33:41 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,23 @@
+## How to run
+```bash
+type "mvn test" in terminal
+```
+## How to use
+
+Pre-Requisites:
+* Java
+* Maven
+
+Features:
+* Selenium
+* POM
+* TestNG
+* Extent Reports
+* Log4j
+
+## References
+* **extentreports-testng-adapter** - [GitHub](https://github.com/extent-framework/extentreports-testng-adapter) 
+* **Extent Framework** - [Documentation](http://www.extentreports.com/docs/versions/4/java/testng.html) 
+
+## Author
+* **Nigel Mulholland** - [Linkedin](https://www.linkedin.com/in/nigel-mulholland/) 
---
+++ src/test/java/selenium4_tests/SlowNetworkTests.java
@@ -0,0 +1,54 @@
+package selenium4_tests;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.devtools.DevTools;
+import org.openqa.selenium.devtools.v84.network.Network;
+import org.openqa.selenium.devtools.v84.network.model.ConnectionType;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.util.Optional;
+
+public class SlowNetworkTests {
+
+    ChromeDriver driver;
+    DevTools devTools;
+
+    @BeforeClass
+    public void setUp () {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        devTools = driver.getDevTools();
+    }
+
+    @Test
+    public void enableSlowNetwork () {
+        devTools.createSession();
+        devTools.send(Network.enable(
+                Optional.empty(),
+                Optional.empty(),
+                Optional.empty()));
+        devTools.send(Network.emulateNetworkConditions(
+                false,
+                150,
+                2500,
+                2000,
+                Optional.of(ConnectionType.CELLULAR3G)));
+        driver.get("https://www.linkedin.com");
+        System.out.println("Slow " + driver.getTitle());
+    }
+
+    @Test
+    public void accessLinkedIn () {
+        driver.get("https://www.linkedin.com");
+        System.out.println("Access " + driver.getTitle());
+    }
+
+    @AfterClass
+    public void tearDown(){
+        driver.quit();
+    }
+}
