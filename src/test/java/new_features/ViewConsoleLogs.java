5388bb9a425e9baa5e526a4bcc4a9dd140766ccc
youvegotnigel <nigeldave19@gmail.com>
2021-02-21 14:17:53 +0530

    add console logs tests

---
+++ src/test/java/new_features/ViewConsoleLogs.java
@@ -0,0 +1,53 @@
+package new_features;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.devtools.DevTools;
+import org.openqa.selenium.devtools.v87.log.Log;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class ViewConsoleLogs {
+
+    private ChromeDriver driver;
+    //private String url = "https://www.lego.com/404";
+    private String url = "https://oneapp.ncinga.com/ba-app/#";
+
+    @BeforeClass
+    public void setUp () {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+
+    @Test
+    public void viewConsoleLogs () {
+        // Get The DevTools And Create A Session
+        DevTools devTools = driver.getDevTools();
+        devTools.createSession();
+
+        // Send A Command To Enable The Logs
+        devTools.send(Log.enable());
+
+        // Add A Listener For The Logs
+        devTools.addListener(Log.entryAdded(), logEntry -> {
+            //System.out.println(logEntry.asSeleniumLogEntry());
+            System.out.println("----------");
+            System.out.println("source = " + logEntry.getSource());
+            System.out.println("level = " + logEntry.getLevel());
+            System.out.println("text = " + logEntry.getText());
+            System.out.println("timestamp = " + logEntry.getTimestamp());
+        });
+
+        // Load The AUT
+        driver.get(url);
+
+    }
+
+
+    @AfterClass
+    public void tearDown(){
+        driver.quit();
+    }
+}
