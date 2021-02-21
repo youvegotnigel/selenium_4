a77dab9828fee019b017d609c415d86cacfc725a
youvegotnigel <nigeldave19@gmail.com>
2021-02-21 16:17:07 +0530

    add new test

---
+++ src/test/java/new_features/UntrustedWebsite.java
@@ -0,0 +1,45 @@
+package new_features;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.devtools.DevTools;
+import org.openqa.selenium.devtools.v84.security.Security;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class UntrustedWebsite {
+
+    private ChromeDriver driver;
+    DevTools devTools;
+
+
+    @BeforeClass
+    public void setUp() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        devTools = driver.getDevTools();
+    }
+
+    @Test
+    public void loadBadWebsite () {
+        devTools.createSession();
+        devTools.send(Security.setIgnoreCertificateErrors(true));
+        driver.get("https://self-signed.badssl.com");
+    }
+
+    @Test
+    public void doNotLoadBadWebsite () {
+        devTools.createSession();
+        devTools.send(Security.setIgnoreCertificateErrors(false));
+        driver.get("https://self-signed.badssl.com");
+    }
+
+    @AfterClass
+    public void tearDown() {
+        driver.quit();
+    }
+
+}
