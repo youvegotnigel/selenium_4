f7ad84e125e4c6b321b32c3f9d1d3bb20ee2b6c8
youvegotnigel <nigeldave19@gmail.com>
2021-02-21 14:21:08 +0530

    add network off test

--- src/test/java/new_features/SlowNetworkTests.java
+++ src/test/java/new_features/SlowNetworkTests.java
@@ -1,6 +1,8 @@
 package new_features;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriverException;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.devtools.DevTools;
 import org.openqa.selenium.devtools.v84.network.Network;
@@ -10,6 +12,9 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import java.util.Optional;
+
+import static org.openqa.selenium.devtools.v87.network.Network.loadingFailed;
+import static org.testng.AssertJUnit.assertEquals;
 
 public class SlowNetworkTests {
 
@@ -45,6 +50,31 @@ public class SlowNetworkTests {
     public void accessLinkedIn () {
         driver.get("https://www.linkedin.com");
         System.out.println("Access " + driver.getTitle());
+    }
+
+    @Test
+    public void enableOfflineNetwork () {
+        devTools.createSession();
+        devTools.send(Network.enable(
+                Optional.empty(),
+                Optional.empty(),
+                Optional.empty()));
+        devTools.send(Network.emulateNetworkConditions(
+                true,
+                10,
+                100,
+                50,
+                Optional.of(ConnectionType.WIFI)));
+        devTools.addListener(loadingFailed(),
+                loadingFailed -> assertEquals(loadingFailed.getErrorText(),
+                        "net::ERR_INTERNET_DISCONNECTED"));
+        try {
+            driver.get("https://www.google.com");
+        } catch (WebDriverException exc) {
+            driver.findElement(By.id("game-button")).click();
+            System.out.println("Offline " + driver.getTitle() + " Page Did Not Load");
+        }
+
     }
 
     @AfterClass
