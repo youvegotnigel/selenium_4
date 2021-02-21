baf3f7d9181c85de292fc96eb86c1bd968c462cf
youvegotnigel <nigeldave19@gmail.com>
2021-02-21 16:19:49 +0530

    small change

--- src/test/java/new_features/UntrustedWebsite.java
+++ src/test/java/new_features/UntrustedWebsite.java
@@ -1,7 +1,6 @@
 package new_features;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.devtools.DevTools;
 import org.openqa.selenium.devtools.v84.security.Security;
