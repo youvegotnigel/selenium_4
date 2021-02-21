bdb9b5df3848b75f917df6cb281b06a7f49c60d5
youvegotnigel <nigeldave19@gmail.com>
2021-02-21 15:25:46 +0530

    add assertion tests

--- src/test/java/new_features/GeoLocation.java
+++ src/test/java/new_features/GeoLocation.java
@@ -1,12 +1,17 @@
 package new_features;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import java.time.Duration;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -14,34 +19,52 @@ public class GeoLocation {
 
     private ChromeDriver driver;
 
+    private double latitude = 7.2008;
+    private double longitude = 79.8737;
+    private double accuracy = 1;
+
+    private By latitudeLocation = By.id("latitude");
+    private By longitudeLocation = By.id("longitude");
+
     @BeforeClass
-    public void setUp () {
+    public void setUp() {
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
         driver.manage().window().maximize();
     }
 
     @Test
-    public void mockGeolocation () {
+    public void mockGeolocation() {
 
-        Map coordinates = new HashMap()
-        {{
-            put("latitude", 7.2008);
-            put("longitude", 79.8737);
-            put("accuracy", 1);
-        }};
-        driver.executeCdpCommand(
-                "Emulation.setGeolocationOverride",coordinates);
+//        Map coordinates = new HashMap() {{
+//            put("latitude", 7.2008);
+//            put("longitude", 79.8737);
+//            put("accuracy", 1);
+//        }};
+
+        Map coordinates = new HashMap();
+
+        coordinates.put("latitude", latitude);
+        coordinates.put("longitude", longitude);
+        coordinates.put("accuracy", accuracy);
+
+        driver.executeCdpCommand("Emulation.setGeolocationOverride", coordinates);
+
         driver.get("https://where-am-i.org/");
-        try {
-            Thread.sleep(10000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+
+        explicitWait(latitudeLocation);
+        Assert.assertEquals(driver.findElement(latitudeLocation).getText(), String.valueOf(latitude));
+        explicitWait(longitudeLocation);
+        Assert.assertEquals(driver.findElement(longitudeLocation).getText(), String.valueOf(longitude));
+    }
+
+    public void explicitWait(By element){
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(element));
     }
 
     @AfterClass
-    public void tearDown(){
+    public void tearDown() {
         driver.quit();
     }
 }
