85191fc1a8a9e078333e9c8723cc26c376876d1f
youvegotnigel <nigeldave19@gmail.com>
2021-03-16 21:20:55 +0530

    take full page screenshot from aShot

--- src/test/java/new_features/PageScreenshot.java
+++ src/test/java/new_features/PageScreenshot.java
@@ -3,6 +3,7 @@ package new_features;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.io.FileHandler;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -10,7 +11,11 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+import ru.yandex.qatools.ashot.AShot;
+import ru.yandex.qatools.ashot.Screenshot;
+import ru.yandex.qatools.ashot.shooting.ShootingStrategies;
 
+import javax.imageio.ImageIO;
 import java.io.File;
 import java.io.IOException;
 import java.time.Duration;
@@ -18,26 +23,46 @@ import java.time.Duration;
 public class PageScreenshot {
 
     private WebDriver driver;
+    private WebDriver driver2;
 
     private By logo = By.id("nav-logo");
 
     @BeforeClass
-    public void setUp () 	{
+    public void setUp() {
+
         WebDriverManager.firefoxdriver().setup();
         driver = new FirefoxDriver();
         driver.manage().window().maximize();
         driver.get("https://www.amazon.com/");
+
+        WebDriverManager.chromedriver().setup();
+        driver2 = new ChromeDriver();
+        driver2.manage().window().maximize();
+        //driver2.get("https://www.airbnb.co.in/s/India/");
+        driver2.get("https://www.amazon.com/");
     }
 
     @Test
-    public void takeFullPageScreenshot () throws IOException {
-        File source = ((FirefoxDriver)driver).getFullPageScreenshotAs(OutputType.FILE);
+    public void takeFullPageScreenshot() throws IOException {
+        File source = ((FirefoxDriver) driver).getFullPageScreenshotAs(OutputType.FILE);
         FileHandler.copy(source, new File("Amazon Full Page Screenshot.png"));
     }
 
     @Test
-    public void takePageScreenshot () throws IOException {
-        File source = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+    public void takeFullPageScreenshot_aShot() {
+
+        //take screenshot of the entire page
+        Screenshot screenshot = new AShot().shootingStrategy(ShootingStrategies.viewportPasting(1000)).takeScreenshot(driver2);
+        try {
+            ImageIO.write(screenshot.getImage(),"PNG", new File(".\\Amazon Full Page Screenshot-2.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void takePageScreenshot() throws IOException {
+        File source = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
         FileUtils.copyFile(source, new File("Amazon Screenshot.png"));
     }
 
@@ -58,7 +83,8 @@ public class PageScreenshot {
     }
 
     @AfterClass
-    public void tearDown () {
+    public void tearDown() {
         driver.quit();
+        driver2.quit();
     }
 }
