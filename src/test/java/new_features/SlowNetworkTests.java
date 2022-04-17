309070af32943dda6b5e77802d3cbb1a54321c74
Nigel Mulholland <nigeldave19@gmail.com>
2022-04-17 22:07:51 +0530

    Update SlowNetworkTests.java

--- src/test/java/new_features/SlowNetworkTests.java
+++ src/test/java/new_features/SlowNetworkTests.java
@@ -6,15 +6,15 @@ import org.openqa.selenium.WebDriverException;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.devtools.DevTools;
 
-import org.openqa.selenium.devtools.v91.network.Network;
-import org.openqa.selenium.devtools.v91.network.model.ConnectionType;
+import org.openqa.selenium.devtools.v99.network.Network;
+import org.openqa.selenium.devtools.v99.network.model.ConnectionType;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import java.util.Optional;
 
-import static org.openqa.selenium.devtools.v91.network.Network.loadingFailed;
+import static org.openqa.selenium.devtools.v99.network.Network.loadingFailed;
 import static org.testng.AssertJUnit.assertEquals;
 
 public class SlowNetworkTests {
