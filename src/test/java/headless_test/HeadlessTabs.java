7c023e286a6d88ce40247299a4846f5459c4635a
Nigel Mulholland <nigeldave19@gmail.com>
2022-08-13 19:15:23 +0530

    Update HeadlessTabs.java

--- src/test/java/headless_test/HeadlessTabs.java
+++ src/test/java/headless_test/HeadlessTabs.java
@@ -2,7 +2,10 @@ package headless_test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.apache.commons.io.FileUtils;
-import org.openqa.selenium.*;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WindowType;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
